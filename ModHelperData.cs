bdd0e9e50176681182dd72a38e5fb8b845c5ce73
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2024-12-11 17:18:14 +0100

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -1,9 +1,9 @@
-﻿namespace Timeskipper
+namespace Timeskipper
 {
     public static class ModHelperData
     {
-        public const string WorksOnVersion = "43.2";
-        public const string Version = "1.0";
+        public const string WorksOnVersion = "46";
+        public const string Version = "1.1";
         public const string Name = "Time Skipper";
         public const string Description = "Rounds Get Rly Boring Sometime? Well why not skip rounds? Credit : DatJaneDoe, 1330 Studio, Thanks datjanedoe for giving me the permission";
         public const string RepoOwner = "Mattcy1";
