6704a76e658c519c97906215b9e2ef0d80dd5a5a
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2024-12-11 17:16:34 +0100

    Update TimeSkipper.cs

--- TimeSkipper.cs
+++ TimeSkipper.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
+using BTD_Mod_Helper;
 using BTD_Mod_Helper.Api;
 using BTD_Mod_Helper.Api.Display;
 using BTD_Mod_Helper.Api.Towers;
@@ -11,51 +12,69 @@ using Il2CppAssets.Scripts.Models.Towers.Behaviors.Abilities.Behaviors;
 using Il2CppAssets.Scripts.Models.Towers.Behaviors.Attack;
 using Il2CppAssets.Scripts.Models.Towers.Weapons;
 using Il2CppAssets.Scripts.Models.TowerSets;
+using Il2CppAssets.Scripts.Simulation.Towers;
 using Il2CppAssets.Scripts.Unity;
 using Il2CppAssets.Scripts.Unity.Display;
 using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using MelonLoader;
+using Timeskipper;
+using Main = TimeSkipper.Main;
+
+[assembly: MelonInfo(typeof(Main), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 
 namespace TimeSkipper
 {
-    public class Skip : ModTowerSet
+    public class Main : BloonsTD6Mod
     {
-        public override string DisplayName => "Skip";
-        public override bool AllowInRestrictedModes => true;
-        public override int GetTowerSetIndex(System.Collections.Generic.List<TowerSet> towerSets) => towerSets.IndexOf(TowerSet.Support) + 1;
-    }
-    public class TimeSkipper : ModTower<Skip>
-    {
-        public override string BaseTower => TowerType.DartMonkey;
-        public override int Cost => 0;
-        public override int TopPathUpgrades => 0;
-        public override int MiddlePathUpgrades => 0;
-        public override int BottomPathUpgrades => 0;
-        public override string Description => "Ever tired of waiting for round to pass? just skip them!";
-        public override string Icon => "Icon";
-        public override string DisplayName => "Time Skipper";
-        public override string Portrait => "Icon";
-        public override void ModifyBaseTowerModel(TowerModel towerModel)
+        public override void OnTowerSelected(Tower tower)
         {
+            if (tower.towerModel.baseId == ModContent.TowerID<TimeSkipperTower>())
             {
-                towerModel.ApplyDisplay<TimeSkipperArt>();
-                towerModel.range = 0;
-                towerModel.footprint = Game.instance.model.GetTowerFromId("DartMonkey").footprint;
-                towerModel.dontDisplayUpgrades = true;
-                towerModel.RemoveBehavior<AttackModel>();
-                List<AbilityModel> abilities = new(0);
-                abilities.Add(new AbilityModel("Skip1", "Skip1", "Skip 1 Round", 0, 0, GetSpriteReference("TimeSkip-1"), 0.1f, null, false, false, null, 0, 0, -1, true, false));
-                abilities.Add(new AbilityModel("Skip10", "Skip10", "Skip 10 Rounds", 0, 0, GetSpriteReference("TimeSkip-10"), 0.1f, null, false, false, null, 0, 0, -1, true, false));
-                abilities.Add(new AbilityModel("Skip100", "Skip100", "Skip 100 Rounds", 0, 0, GetSpriteReference("TimeSkip-100"), 0.1f, null, false, false, null, 0, 0, -1, true, false));
-                abilities.Add(new AbilityModel("SkipMinus1", "SkipMinus1", "Skip -1 Rounds", 0, 0, GetSpriteReference("TimeSkip--1"), 0.1f, null, false, false, null, 0, 0, -1, true, false));
-                abilities.Add(new AbilityModel("SkipCustom", "SkipCustom", "Skip a Custom Amount of Rounds", 0, 0, GetSpriteReference("TimeSkip-Custom"), 0.1f, null, false, false, null, 0, 0, -1, true, false));
-                foreach (var ability in abilities)
+                SkipperUI.CreatePanel();
+            }
+        }
+            
+        public override void OnTowerDeselected(Tower tower)
+        {
+            if (tower.towerModel.baseId == ModContent.TowerID<TimeSkipperTower>())
+            {
+                if (SkipperUI._instance != null)
                 {
-                    towerModel.AddBehavior(ability);
+                    SkipperUI._instance.Close();
                 }
             }
         }
-       public override int ShopTowerCount => 1;
     }
+}
+public class Skip : ModTowerSet
+{
+    public override string DisplayName => "Skip";
+    public override bool AllowInRestrictedModes => true;
+    public override int GetTowerSetIndex(System.Collections.Generic.List<TowerSet> towerSets) => towerSets.IndexOf(TowerSet.Support) + 1;
+}
+public class TimeSkipperTower : ModTower<Skip>
+{
+    public override string BaseTower => TowerType.DartMonkey;
+    public override int Cost => 0;
+    public override int TopPathUpgrades => 0;
+    public override int MiddlePathUpgrades => 0;
+    public override int BottomPathUpgrades => 0;
+    public override string Description => "Ever tired of waiting for round to pass? just skip them!";
+    public override string Icon => "Icon";
+    public override string DisplayName => "Time Skipper";
+    public override string Portrait => "Icon";
+    public override void ModifyBaseTowerModel(TowerModel towerModel)
+    {
+        {
+            towerModel.ApplyDisplay<TimeSkipperArt>();
+            towerModel.range = 0;
+            towerModel.footprint = Game.instance.model.GetTowerFromId("DartMonkey").footprint;
+            towerModel.dontDisplayUpgrades = true;
+            towerModel.RemoveBehavior<AttackModel>();
+        }
+    }
+    public override int ShopTowerCount => 1;
 }
 
 
